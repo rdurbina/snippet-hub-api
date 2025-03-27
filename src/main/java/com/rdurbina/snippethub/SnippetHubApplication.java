8ee273f2cb2b9800a1c4cb03c19bfeee45ba809c
Diego Urbina <diego.urbina1024@gmail.com>
2025-03-27 12:07:19 -0600

    chore: init

---
+++ src/main/java/com/rdurbina/snippethub/SnippetHubApplication.java
@@ -0,0 +1,13 @@
+package com.rdurbina.snippethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnippetHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnippetHubApplication.class, args);
+    }
+
+}
